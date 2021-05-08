6930aa5495e21596eb78a12bc320c5c76a46d403
Francisco Soto <ebobby@ebobby.org>
2021-05-08 00:18:41 -0700

    Convert ticker to a format liked by Alpha Vantage.

--- research/db/model/stock.py
+++ research/db/model/stock.py
@@ -2,6 +2,7 @@
 """Stock/Shares ORM model."""
 
 from orator import Model, SoftDeletes
+from orator.orm import accessor
 
 __author__ = "Francisco Soto"
 
@@ -10,3 +11,18 @@ class Stock(SoftDeletes, Model):
     """Stock/Shares ORM model."""
 
     __dates__ = ["deleted_at"]
+
+    @accessor
+    def ticker_for_alpha_vantage(self):
+        """Convert ticker to a format that's supported by Alpha Vantage."""
+        ticker = self.ticker
+
+        # Alpha Vantage nor Yahoo Finance like dots.
+        ticker = ticker.replace(".", "-")
+
+        # ... nor $, $ becomes -P and every letter after that a -letter.
+        if "$" in ticker:
+            parts = ticker.split("$")
+            ticker = parts[0] + "-P" + "".join([f"-{c}" for c in parts[1]])
+
+        return ticker
